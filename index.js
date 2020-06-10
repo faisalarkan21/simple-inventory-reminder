0193dbfd08cde5276fcc3fa02d3dffb9310e82df
faisal arkan <faisalarkan21@gmail.com>
2020-06-11 01:54:42 +0700

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import ListThumbnailExample from './src/screens/Home/IndexHome';
+
+AppRegistry.registerComponent(appName, () => ListThumbnailExample);
